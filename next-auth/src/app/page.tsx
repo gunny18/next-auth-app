0f2b2f3740cdbeeee31ccc924ca1cb2af3ecc7f4
Ganeshswaminathan R <ganeshswaminathanr@gmail.com>
2024-12-02 19:28:27 +0530

    Starter template

---
+++ next-auth/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <section>
+      <h1>Welcome!</h1>
+      <Button>Click</Button>
+    </section>
+  );
+}
